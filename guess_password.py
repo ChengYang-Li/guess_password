5f5e3bfbd62d254197e4e70d03fcf83a0b81b0bc
Li ChengYang <sinnerunoclay@gmail.com>
2021-07-14 16:08:42 +0800

    you can set password and times

--- guess_password.py
+++ guess_password.py
@@ -1,16 +1,18 @@
 # 猜密碼程式
 
-password = 'a123456'
-chance = 2
+password = input('設定英文+數字7碼密碼： ')
+chance = int(input('你想猜幾次： '))
+remain_chance = chance
 
-while chance >= 0:
+while remain_chance > 0:
 	guess_password = input('請輸入7碼密碼，英文+數字： ')
 	if password != guess_password:
-		if chance == 0:
-			print('密碼錯誤！你已經試了3次了，請1天後再試')
-			break
-		print('密碼錯誤！還有', chance, '次機會')
-		chance -= 1
+		remain_chance -= 1
+		if remain_chance == 0:
+			print('密碼錯誤！你已經試了', chance, '次，請1天後再試')
+		else:
+			print('密碼錯誤！還有', remain_chance, '次機會')
 	else:
 		print('登入成功！')
 		break
+
